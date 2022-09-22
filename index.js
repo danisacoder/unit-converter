c7c6ec618cc4d0dc8ba58476e5a164a9e499d34f
Dan <84781982+danisacoder@users.noreply.github.com>
2022-09-22 06:06:26 -0400

    Added fonts, conversion JS

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertInput = document.getElementById('convert-input')
+const convertBtn = document.getElementById('convert-btn')
+// convertInput.defaultValue = 20
+let convertValue = convertInput.value
+
+const lengthP = document.getElementById('length-p')
+const volumeP = document.getElementById('volume-p')
+const massP = document.getElementById('mass-p')
+
+//  Listen for text field changes and set convertValue to whatever the textbox says
+
+convertInput.addEventListener('change', function() {
+    convertValue = convertInput.value 
+    console.log(convertValue)
+})
+
+// Set the values of all of the div paragraphs to reflect 
+
+convertBtn.addEventListener('click', function() {
+    // console.log('hello')
+
+    lengthP.textContent = `${convertValue} meters = ${(convertValue * 3.281).toFixed(3)} feet | ${convertValue} feet = ${(convertValue / 3.281).toFixed(3)} meters`
+
+    volumeP.textContent = `${convertValue} liters = ${(convertValue * .264).toFixed(3)} gallons | ${convertValue} gallons = ${(convertValue / .264).toFixed(3)} liters`
+
+    massP.textContent = ` ${convertValue} kilograms = ${(convertValue * 2.204).toFixed(3)} pounds | ${convertValue} pounds = ${(convertValue / 2.204).toFixed(3)} kilos`
+
+})
+
---
+++ notes.md
@@ -0,0 +1,1 @@
+[Figma File Copy](https://www.figma.com/file/0fgUfNquSMBV8o3OB94xno/Unit-Conversion-(Copy)?node-id=0%3A13)
